9e8932faac8be5c411230d5a1113fdcafb8c9364
SuRam Noh <shren0705@naver.com>
2023-09-22 11:20:05 +0900

    commit

---
+++ day_15_Hub/src/main/java/com/ezen/cat/score/ScoreDTO.java
@@ -0,0 +1,85 @@
+package com.ezen.cat.score;
+
+public class ScoreDTO {
+	int hakbun;
+	String name;
+	int kor,eng,mat,tot;
+	double avg;
+	String hak;
+	int rank;
+	
+	public ScoreDTO() {	super();	}
+
+	public int getHakbun() {
+		return hakbun;
+	}
+
+	public void setHakbun(int hakbun) {
+		this.hakbun = hakbun;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public String getHak() {
+		return hak;
+	}
+
+	public void setHak(String hak) {
+		this.hak = hak;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+}
